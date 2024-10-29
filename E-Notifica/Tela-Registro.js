2e3adf2533e58b75441758496f3da04106e52ded
Fábio <fabioaugusto1767@gmail.com>
2024-10-28 21:24:21 -0300

    Adição da tela de registro e centralização da tela de login estudante

---
+++ E-Notifica/Tela-Registro.js
@@ -0,0 +1,8 @@
+function confirmar () {
+    let ra = document.getElementById('ra').value;
+    localStorage.setItem('RaUsuario', ra);
+  }
+  
+  document.getElementById('acessar').addEventListener("click",function(){
+    window.location.href = "Tela-Principal.html"
+  })
