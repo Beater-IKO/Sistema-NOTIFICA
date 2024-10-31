7024f5101544cbf57823e38148450a875ee5bcf5
Beater-IKO <samuelsilva.wly@gmail.com>
2024-10-31 19:44:30 -0300

    falta fazer os botões inflar

---
+++ E-Notifica/script.js
@@ -0,0 +1,34 @@
+document.getElementById('criar-ticket').addEventListener("click",function(){
+
+    location.href = "Criar-tickets.html"
+
+})
+
+
+document.getElementById('ticket-em-andamento').addEventListener("click",function(){
+
+    location.href = "Tickets-Em-Andamentos.html"
+
+})
+
+document.getElementById('tickets-finalizados').addEventListener("click",function(){
+
+    location.href = "Tickets-Finalizados.html"
+
+})
+
+
+document.getElementById('historicos-de-tickets').addEventListener("click",function(){
+
+    location.href = "Histórico-de-Tickets.html"
+
+})
+
+
+document.getElementById('icone-suporte').addEventListener("click",function(){
+
+    location.href = "Criar-tickets.html"
+
+})
+
+
