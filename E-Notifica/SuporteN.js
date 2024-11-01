7bd6d064cff79e3fbd4cad647579bec0f405abba
Erick Gustavo Tonello <erick.tonello@yahoo.com>
2024-11-01 18:32:47 -0300

    Suporte Final

---
+++ E-Notifica/SuporteN.js
@@ -0,0 +1,7 @@
+document.getElementById('sla').addEventListener("click",function(){
+    window.location.href = "Tela-Principal.html"
+  })
+
+  document.getElementById('Suporte1').addEventListener("click",function(){
+    window.location.href = "SuporteN.html"
+  })
