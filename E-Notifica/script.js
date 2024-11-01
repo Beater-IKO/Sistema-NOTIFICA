3ab9138a7bde6392410e637061eab5866e6446c4
Beater-IKO <samuelsilva.wly@gmail.com>
2024-11-01 01:49:24 -0300

    Botões inflado e mais.
    Os botões da pagina de criar tickets estão inflando quando selecionados, arrumei detalhes de quando você entra numa página a opção dela fique maior, adicionei um link no logo da uniamerica para voltar pro menu principal e linkei as outras opçoes certo.

--- E-Notifica/script.js
+++ E-Notifica/script.js
@@ -1,3 +1,10 @@
+document.getElementById('logo').addEventListener("click",function(){
+
+    location.href = "Tela-Principal.html"
+
+})
+
+
 document.getElementById('criar-ticket').addEventListener("click",function(){
 
     location.href = "Criar-tickets.html"
@@ -5,9 +12,9 @@ document.getElementById('criar-ticket').addEventListener("click",function(){
 })
 
 
-document.getElementById('ticket-em-andamento').addEventListener("click",function(){
+document.getElementById('ticket-em-andamento').addEventListener("click", function(){
 
-    location.href = "Tickets-Em-Andamentos.html"
+    location.href = "Tickets-Em-Andamento.html"
 
 })
 
@@ -31,10 +38,18 @@ document.getElementById('icone-suporte').addEventListener("click",function(){
 
 })
 
+document.getElementById('icone-suporte').addEventListener("click",function(){
+
+    location.href = "SuporteN.html"
+
+})
+
 
 
 
 let opcaoInflada = null;
+let opcaoInfladaAndar = null;
+let opcaoInfladaGrau = null;
 
 function inflar(interna) {
     if (opcaoInflada) {
@@ -45,6 +60,157 @@ function inflar(interna) {
         interna.classList.add('inflada');
         opcaoInflada = interna;
     } else {
-        opcaoInflada = null; // se clicar na mesma, desfaz a inflada
+        opcaoInflada = null; 
     }
 }
+
+//
+
+function inflar(externa) {
+    if (opcaoInflada) {
+        opcaoInflada.classList.remove('inflada');
+    }
+
+    if (externa !== opcaoInflada) {
+        externa.classList.add('inflada');
+        opcaoInflada = externa;
+    } else {
+        opcaoInflada = null; 
+    }
+}
+
+
+//-----------------------------------------------
+
+
+function inflarAndar(andar1) {
+
+
+    if (opcaoInfladaAndar) {
+        opcaoInfladaAndar.classList.remove('inflada');
+    }
+
+    if (andar1 !== opcaoInfladaAndar) {
+        andar1.classList.add('inflada');
+        opcaoInfladaAndar = andar1;
+    } else {
+        opcaoInfladaAndar = null; 
+    }
+}
+
+function inflarAndar(andar2) {
+
+
+    if (opcaoInfladaAndar) {
+        opcaoInfladaAndar.classList.remove('inflada');
+    }
+
+    if (andar2 !== opcaoInfladaAndar) {
+        andar2.classList.add('inflada');
+        opcaoInfladaAndar = andar2;
+    } else {
+        opcaoInfladaAndar = null; 
+    }
+}
+
+
+function inflarAndar(andar3) {
+
+
+    if (opcaoInfladaAndar) {
+        opcaoInfladaAndar.classList.remove('inflada');
+    }
+
+    if (andar3 !== opcaoInfladaAndar) {
+        andar3.classList.add('inflada');
+        opcaoInfladaAndar = andar3;
+    } else {
+        opcaoInfladaAndar = null; 
+    }
+}
+
+
+function inflarAndar(andar4) {
+
+
+    if (opcaoInfladaAndar) {
+        opcaoInfladaAndar.classList.remove('inflada');
+    }
+
+    if (andar4 !== opcaoInfladaAndar) {
+        andar4.classList.add('inflada');
+        opcaoInfladaAndar = andar4;
+    } else {
+        opcaoInfladaAndar = null; 
+    }
+}
+
+
+
+//
+
+
+function inflarGrau(grauleve) {
+
+
+    if (opcaoInfladaGrau) {
+        opcaoInfladaGrau.classList.remove('inflada');
+    }
+
+    if (grauleve !== opcaoInfladaGrau) {
+        grauleve.classList.add('inflada');
+        opcaoInfladaGrau = grauleve;
+    } else {
+        opcaoInfladaGrau = null; 
+    }
+}
+
+
+function inflarGrau(graumediana) {
+
+
+    if (opcaoInfladaGrau) {
+        opcaoInfladaGrau.classList.remove('inflada');
+    }
+
+    if (graumediana !== opcaoInfladaGrau) {
+        graumediana.classList.add('inflada');
+        opcaoInfladaGrau = graumediana;
+    } else {
+        opcaoInfladaGrau = null; 
+    }
+}
+
+
+function inflarGrau(graugrave) {
+
+
+    if (opcaoInfladaGrau) {
+        opcaoInfladaGrau.classList.remove('inflada');
+    }
+
+    if (graugrave !== opcaoInfladaGrau) {
+        graugrave.classList.add('inflada');
+        opcaoInfladaGrau = graugrave;
+    } else {
+        opcaoInfladaGrau = null; 
+    }
+}
+
+
+
+function inflarurgencia(grauurgencia) {
+
+
+    if (opcaoInfladaGrau) {
+        opcaoInfladaGrau.classList.remove('inflada');
+    }
+
+    if (grauurgencia !== opcaoInfladaGrau) {
+        grauurgencia.classList.add('inflada');
+        opcaoInfladaGrau = grauurgencia;
+    } else {
+        opcaoInfladaGrau = null; 
+    }
+}
+
